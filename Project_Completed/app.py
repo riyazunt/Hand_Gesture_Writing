4225fea6f53933fef320fabf2eeb77f592c3223c
riyazunt <riyazunt@gmail.com>
2022-10-14 21:55:17 +0530

    modified

--- Project_Completed/app.py
+++ Project_Completed/app.py
@@ -7,6 +7,7 @@ import math
 import threading
 import time
 import sys 
+import tensorflow as tf
 
 # st.set_page_config(layout="wide")
 
@@ -16,12 +17,15 @@ run = st.checkbox('Click check box to start camera')
 frame_window = st.image([])
 
 detector = HandDetector(maxHands=1)
-classifier = Classifier("model/keras_model.h5", "model/labels.txt")
+
+model = tf.keras.models.load_model('model/transferlearned_gesture.h5')
+# model = Classifier("model/keras_model.h5", "model/labels.txt")
 
 offset = 20
-imgSize = 300
+imgSize = 224
  
-labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'DELETE', 'SPACE', 'CLEAR']
+# labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'DELETE', 'SPACE', 'CLEAR']    
+randLabels = ['A','B','K','L','M','N','O','P','Q','R','S','T','C','U','V','W','X','Y','Z','DELETE','SPACE','CLEAR','D','E','F','G','H','I','J']
 text = ''
 error = ''
 
@@ -30,19 +34,21 @@ er = st.text(error)
 
 def predict(*args):
     
-    global pred, timer
-    pred = True
+    global bool_val, timer
+    bool_val = True
     time.sleep(0.05)
-    pred = False
+    bool_val = False
     timer = threading.Timer(3, predict)
     timer.start()    
 
 predict() 
 
 cam = cv2.VideoCapture(0) 
+
 while run:
+  
     try:
-        ret, img = cam.read() 
+        ret, img = cam.read()
         imgOutput = img.copy()
         hands, img = detector.findHands(img)
         out = img.copy()
@@ -64,13 +70,13 @@ while run:
             cv2.rectangle(imgOutput, (x-offset, y-offset),(x + w+offset, y + h+offset), (0, 0, 255), cv2.FILLED)
             # cv2.putText(out, text, (50,100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1) 
 
-        if pred and hands:
-            
+        if bool_val and hands:
+
             alpha = 0.01
             mask = imgOutput.astype(bool)
             out[mask] = cv2.addWeighted(img, alpha, imgOutput, 1 - alpha, 0)[mask]
             frame_window.image(cv2.cvtColor(out, cv2.COLOR_BGR2RGB))
-            
+
             if aspectRatio > 1:
                 k = imgSize / h
                 wCal = math.ceil(k * w)
@@ -78,7 +84,15 @@ while run:
                 imgResizeShape = imgResize.shape
                 wGap = math.ceil((imgSize - wCal) / 2)
                 imgWhite[:, wGap:wCal + wGap] = imgResize
-                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+                norm=imgWhite/255.0
+                reshaped=np.reshape(norm,(1,224,224,3))
+                reshaped = np.vstack([reshaped])
+
+#                 pred, index = model.getPrediction(imgWhite, draw=False)
+
+                pred = model.predict(reshaped)
+                index = np.argmax(pred)
 
             else:
                 k = imgSize / w
@@ -87,21 +101,29 @@ while run:
                 imgResizeShape = imgResize.shape
                 hGap = math.ceil((imgSize - hCal) / 2)
                 imgWhite[hGap:hCal + hGap, :] = imgResize
-                prediction, index = classifier.getPrediction(imgWhite, draw=False)
-            
-            
+
+                norm=imgWhite/255.0
+                reshaped=np.reshape(norm,(1,224,224,3))
+                reshaped = np.vstack([reshaped])
+
+#                 pred, index = model.getPrediction(imgWhite, draw=False)
+
+                pred = model.predict(reshaped)
+                index = np.argmax(pred)
+
+
             def update(text):
-                if labels[index] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-                    text = text+labels[index]
-                elif labels[index] == 'DELETE' and text:
+                if randLabels[index] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+                    text = text+randLabels[index]
+                elif randLabels[index] == 'DELETE' and text:
                     text = text[:-1]
-                elif labels[index] == 'SPACE':
+                elif randLabels[index] == 'SPACE':
                     text = text+' '
-                elif labels[index] == 'CLEAR':
+                elif randLabels[index] == 'CLEAR':
                     text = ''
                 else:
                     pass
-                    
+
                 return text
 
             text = update(text)
@@ -109,17 +131,18 @@ while run:
             s.write(f"<h1 style='color:black;'>{text}</h1>", unsafe_allow_html=True)
             error = 'reading success'
             er.write(f"<p style='color:green;'>{error}</p>", unsafe_allow_html=True) 
-            
-            
+
+
         else:  
             frame_window.image(cv2.cvtColor(out, cv2.COLOR_BGR2RGB))
 
-    except: 
-        error = 'error occured in reading image, restart camera and keep the hand within window'
+    except:
+        error = 'Either camera turned off or an error occured in reading image, \
+               \nrestart camera and keep the hand within window'
         er.write(f"<p style='color:red;'>{error}</p>", unsafe_allow_html=True)
         break
 
-# else:
+        
 cam.release() 
 timer.cancel()
 frame_window.image([])
